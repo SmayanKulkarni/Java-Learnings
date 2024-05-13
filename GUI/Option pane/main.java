93c838dfb7942a23d17c1f0437a7423a718efe59
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-05-13 11:44:36 +0530

    .

--- GUI/Option pane/main.java
+++ GUI/Option pane/main.java
@@ -9,5 +9,18 @@ class main{
         JOptionPane.showMessageDialog(null,"useless information", "title", JOptionPane.INFORMATION_MESSAGE); 
         JOptionPane.showMessageDialog(null,"useless information", "title", JOptionPane.QUESTION_MESSAGE); 
         JOptionPane.showMessageDialog(null,"useless. information", "title", JOptionPane.WARNING_MESSAGE); 
+
+        int ans = JOptionPane.showConfirmDialog(null, "bro what are u doing", "title", JOptionPane.YES_NO_CANCEL_OPTION);
+        String name = JOptionPane.showInputDialog("What is your name?");
+        JOptionPane.showConfirmDialog(null, "is your name really " +name +"?", "Confirmation  ", JOptionPane.YES_NO_CANCEL_OPTION);
+        System.out.println("Hello, "+name+"!");
+
+        String responses[] = { "no, youre awesome", "thank you", "bro what"};
+        ImageIcon image = new ImageIcon("/home/smayan/Desktop/Java/GUI/Buttons/360_F_203250153_gYJH96UPlNrNwpR56ABJWi7k5HRvPaGw.jpg");
+        JOptionPane.showOptionDialog(null, "these are the options", 
+            "secret message", JOptionPane.YES_NO_CANCEL_OPTION,         
+            JOptionPane.INFORMATION_MESSAGE,
+            image, responses, 0);
+
     }
 }
