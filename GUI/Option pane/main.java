ea8d5389af3265ba91430c550bb7eae934da2210
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-05-13 10:40:59 +0530

    .

---
+++ GUI/Option pane/main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+class main{
+    public static void main(String args[])
+    {
+        JOptionPane.showMessageDialog(null,"useless information", "title", JOptionPane.ERROR_MESSAGE); 
+        JOptionPane.showMessageDialog(null,"useless information", "title", JOptionPane.INFORMATION_MESSAGE); 
+        JOptionPane.showMessageDialog(null,"useless information", "title", JOptionPane.QUESTION_MESSAGE); 
+        JOptionPane.showMessageDialog(null,"useless. information", "title", JOptionPane.WARNING_MESSAGE); 
+    }
+}
