863629eea4630cec13e7b11760e070ffea6b4c9a
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-05-13 12:30:57 +0530

    .

---
+++ GUI/radio buttons/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+    public static void main(String[] args) {
+    
+     // JRadioButton = One or more buttons in a grouping in which only 1 may be selected per group
+     
+     new MyFrame();
+   
+    }
+   }
